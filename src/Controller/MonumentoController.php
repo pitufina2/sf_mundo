9e3137a651ec88d597d0e03297a0108ed3cc3ad4
maricarmen <maricarmen>
2018-05-29 20:37:47 +0200

    otra version de ejercicio del proyecto mundo con monumento

--- src/Controller/MonumentoController.php
+++ src/Controller/MonumentoController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Monumento;
 use App\Form\MonumentoType;
 use App\Repository\MonumentoRepository;
@@ -9,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 /**
  * @Route("/monumento")
  */
@@ -22,7 +19,6 @@ class MonumentoController extends Controller
     {
         return $this->render('monumento/index.html.twig', ['monumentos' => $monumentoRepository->findAll()]);
     }
-
     /**
      * @Route("/new", name="monumento_new", methods="GET|POST")
      */
@@ -31,21 +27,17 @@ class MonumentoController extends Controller
         $monumento = new Monumento();
         $form = $this->createForm(MonumentoType::class, $monumento);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($monumento);
             $em->flush();
-
             return $this->redirectToRoute('monumento_index');
         }
-
         return $this->render('monumento/new.html.twig', [
             'monumento' => $monumento,
             'form' => $form->createView(),
         ]);
     }
-
     /**
      * @Route("/{id}", name="monumento_show", methods="GET")
      */
@@ -53,7 +45,6 @@ class MonumentoController extends Controller
     {
         return $this->render('monumento/show.html.twig', ['monumento' => $monumento]);
     }
-
     /**
      * @Route("/{id}/edit", name="monumento_edit", methods="GET|POST")
      */
@@ -61,19 +52,15 @@ class MonumentoController extends Controller
     {
         $form = $this->createForm(MonumentoType::class, $monumento);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
-
             return $this->redirectToRoute('monumento_edit', ['id' => $monumento->getId()]);
         }
-
         return $this->render('monumento/edit.html.twig', [
             'monumento' => $monumento,
             'form' => $form->createView(),
         ]);
     }
-
     /**
      * @Route("/{id}", name="monumento_delete", methods="DELETE")
      */
@@ -84,7 +71,6 @@ class MonumentoController extends Controller
             $em->remove($monumento);
             $em->flush();
         }
-
         return $this->redirectToRoute('monumento_index');
     }
 }
--- src/Controller/PaisController.php
+++ src/Controller/PaisController.php
@@ -1,17 +1,19 @@
 <?php
-
 namespace App\Controller;
-
-use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Pais;
 use App\Form\PaisType;
+use App\Entity\Region;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
     /**
      * @Route("/pais")
      */
-
 class PaisController extends Controller
 {
     /**
@@ -19,46 +21,63 @@ class PaisController extends Controller
      */
     public function index(Request $request)
     {
-        $pais = new Pais();
+        $pais = new Pais ();
         $formu = $this->createForm(PaisType::class, $pais);
         $formu->handleRequest($request);
-
         if ($formu->isSubmitted()) {
-
             $em = $this->getDoctrine()->getManager();
-
             $em->persist($pais);
-
             $em->flush();
-
-
+            
             return $this->redirectToRoute('pais_lista');
-          
         }
-
-        return $this->render('pais/nuevo.html.twig', [
-            'formulario' => $formu->createView(),
+            return $this->render('pais/nuevo.html.twig', [
+            'formulario' => $formu ->createView(),
         ]);
-        
     }
-
     /**
      * @Route("/lista", name="pais_lista")
      */
     public function listado()
     {
-
-        //$this->cargarDatos();
-        $repo = $this->getDoctrine()->getRepository (Pais::class);
-
-        $paises = $repo->findAll();    
-
-     
-
-        return $this->render('pais/index.html.twig', [
-            'paises' => $paises,
-             
-            
+        $repo = $this->getDoctrine()->getRepository(Pais::class);
+        
+        $paises = $repo->findAll();
+        
+            return $this->render ('pais/index.html.twig', [
+            'paises' =>  $paises,
         ]);
+    }
+    /**
+     * @Route("/detalle/{id}", name="pais_detalle", requirements={"id"="\d+"})
+     */
+    public function detalle($id)
+    {
+        $repo = $this->getDoctrine()->getRepository(Pais::class);
+        
+        $pais = $repo->find($id);
+                   
+            return $this->render ('pais/detalle.html.twig', [
+            'pais' =>  $pais,
+        ]);
+    }
+    /**
+     * @Route("/jsonlist", name="pais_jsonlist")
+     */
+    public function jsonRegiones()
+    {
+        $encoder = new JsonEncoder();
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceHandler(
+            function ($object) {
+                return $object->getId();
+            }
+        );
+        $serializer = new Serializer(array($normalizer), array($encoder));
+        $repo = $this->getDoctrine()->getRepository(Pais::class);
+        $paises = $repo->findAll();
+        $jsonPaises = $serializer->serialize($paises, 'json');        
+        $respuesta = new Response($jsonPaises);
+        return $respuesta;
     }
 }
